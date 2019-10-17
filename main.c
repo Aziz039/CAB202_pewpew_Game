34796fcc4a8e322b5de3c7a4d0dc5c90fe91f0d7
Abdulaziz Ali <zizogmd@gmail.com>
2019-10-17 21:24:29 +1000

    Added cheese
    Added cheese interaction

--- main.c
+++ main.c
@@ -34,8 +34,18 @@ uint8_t jerry[5] = {
     0b11010,
     0b01100
 };
+
+uint8_t cheese[5] = {
+    0b01111,
+    0b11000,
+    0b10000,
+    0b11000,
+    0b01111
+};
+
 uint8_t jerryDirected[5];
 uint8_t tomDirected[5];
+uint8_t cheeseDirected[5];
 
 int jerryX = 0;
 int jerryY = 9;
@@ -43,6 +53,8 @@ int jerryY = 9;
 int tomX = LCD_X - 8; // 8 is bitmap size
 int tomY = LCD_Y - 9; // 8 is bitmap size
 int tomeSideDirection;
+
+int cheeseLocation[5][2];
 
 // joystic
 int joystick_l;
@@ -66,6 +78,10 @@ int tomTimerCounter = 0; //counter for Tom timer
 
 bool gamePaused = false;
 
+int maxNumOfCheese = 5;
+int numOfCheeseOnGame = 0;
+bool drawCheeseTimer = false;
+
 // walls
 int walls[][4] = {
     {18,15,13,25},
@@ -77,6 +93,8 @@ int numOfWalls = 4;
 
 
 void moveTom();
+double CalcDistanceBetween2Points(int x1, int y1, int x2, int y2);
+double PointLinesOnLine(int x, int y, int x1, int y1, int x2, int y2, double allowedDistanceDifference);
 
 //****************************************************//
 // Starter functions                                  //
@@ -93,6 +111,8 @@ void setup_draw(void) {
             WRITE_BIT(jerryDirected[i], j, jerry_bit_val);
             uint8_t tom_bit_val = BIT_VALUE(tom[j], (4 - i));
             WRITE_BIT(tomDirected[i], j, tom_bit_val);
+            uint8_t cheese_bit_val = BIT_VALUE(cheese[j], (4 - i));
+            WRITE_BIT(cheeseDirected[i], j, cheese_bit_val);
         }
     }
 
@@ -186,10 +206,92 @@ void drawWalls() {
     }
 }
 
+bool overlapWithWalls(int x, int y) {
+    // Check if cheese overlap with all walls
+    for (int i = 0; i < numOfWalls; i++) {
+        if (PointLinesOnLine(x, y, walls[i][0],walls[i][1],walls[i][2],walls[i][3], 10e-5) == 1 ||
+        PointLinesOnLine(x + 4, y + 4, walls[i][0],walls[i][1],walls[i][2],walls[i][3], 10e-5) == 1 ||
+        PointLinesOnLine(x, y + 4, walls[i][0],walls[i][1],walls[i][2],walls[i][3], 10e-5) == 1 ||
+        PointLinesOnLine(x + 4, y, walls[i][0],walls[i][1],walls[i][2],walls[i][3], 10e-5) == 1
+        ) {
+            return true;
+        }
+    } 
+    return false;
+}
+
+bool overlapWithCharacter(int x, int y) {
+    // check if cheese overlap Tom
+    if (PointLinesOnLine(x, y, tomX, tomY ,tomX + 4 , tomY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(x + 4, y + 4, tomX, tomY ,tomX + 4 , tomY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(x, y + 4, tomX, tomY ,tomX + 4 , tomY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(x + 4, y, tomX, tomY ,tomX + 4 , tomY + 4, 10e-5) == 1
+        ) {
+            return true;
+    }
+
+    // check if cheese overlap Jerry
+    if (PointLinesOnLine(x, y, jerryX, jerryY ,jerryX + 4 , jerryY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(x + 4, y + 4, jerryX, jerryY ,jerryX + 4 , jerryY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(x, y + 4, jerryX, jerryY ,jerryX + 4 , jerryY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(x + 4, y, jerryX, jerryY ,jerryX + 4 , jerryY + 4, 10e-5) == 1
+        ) {
+            return true;
+    }
+    return false;
+}
+
+bool checkCheesePosition(int x, int y) {
+    // check if cheese overlapp any other object
+    if (!overlapWithWalls(x, y)) { // check if overlap with wall
+        if (!overlapWithCharacter(x,y)) { // check if overlap with character
+            return true;
+        }
+    }
+    return false;
+}
+
+void drawCheese() {
+    if ((clock_second + 1) % 2 == 0) {
+        drawCheeseTimer = true;
+    }
+    if (clock_second % 2 == 0 && drawCheeseTimer) {
+        if (numOfCheeseOnGame < maxNumOfCheese) { // check if number of cheese less than maximum
+            int x = rand() % 84; // create random x variable for cheese location
+            int y = (rand() % 40) + 8; // create randomm y variable for cheese location
+            if (checkCheesePosition(x, y)) { // check if cheese overlapp any other object
+                cheeseLocation[numOfCheeseOnGame][0] = x;
+                cheeseLocation[numOfCheeseOnGame][1] = y;
+                numOfCheeseOnGame++;   
+                drawCheeseTimer = false;
+            }
+        }
+    }
+    for (int i = 0; i < numOfCheeseOnGame; i++) {
+        draw_data(cheeseLocation[i][0], cheeseLocation[i][1], cheeseDirected);
+    }
+}
+
+void didJerryAteTheCheese() {
+    for (int i = 0; i < numOfCheeseOnGame; i++) {
+        if (PointLinesOnLine(cheeseLocation[i][0], cheeseLocation[i][1], jerryX, jerryY ,jerryX + 4 , jerryY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(cheeseLocation[i][0] + 4, cheeseLocation[i][1] + 4, jerryX, jerryY ,jerryX + 4 , jerryY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(cheeseLocation[i][0], cheeseLocation[i][1] + 4, jerryX, jerryY ,jerryX + 4 , jerryY + 4, 10e-5) == 1 ||
+        PointLinesOnLine(cheeseLocation[i][0] + 4, cheeseLocation[i][1], jerryX, jerryY ,jerryX + 4 , jerryY + 4, 10e-5) == 1
+        ) {
+            cheeseLocation[i][0] = cheeseLocation[numOfCheeseOnGame - 1][0];
+            cheeseLocation[i][1] = cheeseLocation[numOfCheeseOnGame - 1][1];
+            numOfCheeseOnGame--;
+            gameScore++;
+        }
+    }
+}
+
 
 void process_helper_drawing() {
     clear_screen();
     drawWalls();
+    drawCheese();
     draw_data(jerryX, jerryY, jerryDirected);
     draw_data(tomX, tomY, tomDirected);
     moveTom();
